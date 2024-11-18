a03ca346a81793298f8b4d98afc63dd7e05c4cc5
hadis2000 <hadisehoseini6301@gmail.com>
2024-11-18 12:36:41 +0330

    .

--- src/pages/plants-detail/index.tsx
+++ src/pages/plants-detail/index.tsx
@@ -1,7 +1,16 @@
+import Footer from "../../compopnent/footer";
+import Slider from "../landing/slider";
 import Info from "./info";
 
 const PlantsDetails = () => {
-  return <Info />;
+  return (
+    <>
+      <Info />
+      <Slider />
+
+      <Footer />
+    </>
+  );
 };
 
 export default PlantsDetails;
