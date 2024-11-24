f4f8bd7115a93ab95f6b8d7984d87015ace33d5d
hadis2000 <hadisehoseini6301@gmail.com>
2024-11-24 10:04:03 +0330

    delete swiper

--- src/test/index.tsx
+++ src/test/index.tsx
@@ -1,37 +1,9 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper/modules";
 import { Box } from "@mui/material";
 
 const Test = () => {
-  const slides = ["Slide 1", "Slide 2", "Slide 3", "Slide 4"];
   return (
     <Box sx={{ width: "100%", maxWidth: "600px", margin: "0 auto" }}>
-      <Swiper
-        modules={[Navigation, Pagination]}
-        navigation
-        pagination={{ clickable: true }}
-        spaceBetween={20}
-        slidesPerView={1}
-      >
-        {slides.map((slide, index) => (
-          <SwiperSlide key={index}>
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "200px",
-                backgroundColor: "primary.main",
-                color: "white",
-                fontSize: "1.5rem",
-                borderRadius: "8px",
-              }}
-            >
-              {slide}
-            </Box>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      ddddddd
     </Box>
   );
 };
